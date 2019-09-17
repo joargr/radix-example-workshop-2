6cd1a1636160fc894794fdc221157c96c7642fcc
Joar Grimstvedt <joarg@equinor.com>
2019-09-17 15:01:26 +0200

    Added echo app to application

--- www/src/App.js
+++ www/src/App.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import Echo from './components/Echo';
-import Header from './components/Header';
-import EchoService from './services/Echo';
-import './App.css';
+import React from "react";
+import Echo from "./components/Echo";
+import Header from "./components/Header";
+import EchoService from "./services/Echo";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.refreshInterval = 1000;
-    this.useMockData = true;
-    
-    this.state ={
+    this.useMockData = false;
+
+    this.state = {
       echoResult: {},
-      nrRefresh: 0
-    }
+      nrRefresh: 0,
+    };
   }
 
   componentDidMount() {
-    setInterval(this.loadData, this.refreshInterval) 
+    setInterval(this.loadData, this.refreshInterval);
   }
 
   loadData = () => {
-    const echoService = EchoService(this.useMockData)
-    const nrRefresh = this.state.nrRefresh + 1
+    const echoService = EchoService(this.useMockData);
+    const nrRefresh = this.state.nrRefresh + 1;
 
-    echoService.fetch('/api/echo').then(data => this.setState({ echoResult: data, nrRefresh: nrRefresh}))
-  }
-  
+    echoService
+      .fetch("/api/echo")
+      .then(data => this.setState({ echoResult: data, nrRefresh: nrRefresh }));
+  };
+
   render() {
     return (
       <div className="App">
         <Header />
-        <Echo result={this.state.echoResult} refreshInterval={this.refreshInterval} nrRefresh={this.state.nrRefresh} />
+        <Echo
+          result={this.state.echoResult}
+          refreshInterval={this.refreshInterval}
+          nrRefresh={this.state.nrRefresh}
+        />
       </div>
     );
   }
