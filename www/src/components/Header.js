3ba10525f4744d019c63594560c431530aa3b11c
Joar Grimstvedt <joarg@equinor.com>
2019-09-17 14:36:17 +0200

    Change of www app name and a minor title tweak

--- www/src/components/Header.js
+++ www/src/components/Header.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "./logo.svg";
 
 const Header = () => {
-  return <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Radix a PaaS built on Kubernetes - Coool
-    </p>
-    <div className="App-description">
-    <br/>
-    Radix is an application platform built to increase development speed by providing  
-    <br />
-    the best possible tools to develop and run your applications.
-    </div>
-    <a
-      className="App-link"
-      href="https://www.radix.equinor.com"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn more
-    </a>
-  </header>
-}
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Radix a PaaS built on Kubernetes - Cool</p>
+      <div className="App-description">
+        <br />
+        Radix is an application platform built to increase development speed by
+        providing
+        <br />
+        the best possible tools to develop and run your applications.
+      </div>
+      <a
+        className="App-link"
+        href="https://www.radix.equinor.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn more
+      </a>
+    </header>
+  );
+};
 
 export default Header;
